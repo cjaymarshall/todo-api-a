2fcb7bcb8257e661ebe074e9173751bf1636937b
cjaymarshall <cjaymarshall1@hotmail.com>
2019-03-03 10:32:34 -0700

    added delete route

--- server.js
+++ server.js
@@ -37,7 +37,19 @@ app.post('/todos', function(req,res) {
     body.id = todoNextId++;
     todos.push(body);
     res.json(body);
-})
+});
+
+app.delete('/todos/:id', function(req,res) {
+    var todoId = parseInt(req.params.id);
+    var matchedTodo = _.findWhere(todos, {id:todoId});
+
+    if(!matchedTodo) {
+        res.status(404).json({"error" : "no todo with that id"});
+    } else {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    }
+});
 
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + "!");
